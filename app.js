fb56ea432f240aaf19e50fa7da32ee3c75aac2bf
nadimkhankpk <nadim.khankpk99@gmail.com>
2020-06-15 20:43:00 +0500

    assigmnet5

---
+++ app.js
@@ -0,0 +1,64 @@
+// chapter #01 
+// task#1 alert("Error! Please enter a valid passward")
+// task #2 alert("Welcome to JS Land \n Happy Coding!")
+// task#3 alert(" Happy Coding! \n preventthis page from creating additional dialogs")
+ //task #4 console.log("Hello ..... I can run JS through my web browser console")
+
+
+// chapter# 2
+//task #1var username;
+//task# 2 var myname="Nadim Khan";
+// task# 3
+//var message;
+// message="hello world";
+// alert(message);
+
+
+// task 4
+// var name= "nadim"
+//  var age= 23;
+//  var course="web and app";
+//  alert(name);
+//  alert(age);
+//  alert(course);
+// task #5 
+// var name="pizza\npizz\npiz\npi\ni"
+//  alert(name);
+
+// chapter # 3
+// task #1   
+//var age= 23;
+// alert("i am "+age+" old");
+
+// //task #2
+// var bdate=1997;
+// document.write("my birth year is"+bdate+"<br>")
+// document.write( "data type of  my declred variable is number")
+
+
+// task#3
+// var name =" <b>Nadim</b>"
+// var product="<b> T SHIRT</b>"
+// var qaunt= "<b>5</b>";
+// document.write(name+" order "+ qaunt+" " + product+"(s) on www.xyzclothingstore");
+
+//chapter # 4
+
+// task# 1
+// var x,y ,z;
+
+// task #2
+// var x="name";
+// var x1=" value";
+//  var first_name=" nadim"
+// var first-name2="name"
+
+
+//task # 3
+// document.write("<h1> Rules for naming JS variables </h1> <br>");
+// document.write("Variable names can only contain, numbers $ and -- For example $my_1stVariable <br>");
+// document.write("Variables must begin with a ______, ______ or _____. For example $name, _name or name <br>")
+//  document.write("Variable names are case _________<br>")
+
+// chapter# 5
+
